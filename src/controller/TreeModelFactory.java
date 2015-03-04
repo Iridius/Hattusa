62b7f9a6772182f511f4ff2856a51dccbb2af56e
Iridius <irde@mail.ru>
2015-03-04 22:57:04 +0300

    M: template for Tree control source.

---
+++ src/controller/TreeModelFactory.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.ITreeElement;
+
+import java.util.Collection;
+
+public class TreeModelFactory {
+    public TreeModel create(Collection<ITreeElement> elements) {
+
+    }
+}
