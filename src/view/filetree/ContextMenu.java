39d69d5b44f964338104d640d8af3bfebeba82a4
Iridius <irde@mail.ru>
2015-03-13 22:47:20 +0300

    M: FileTree: some changes in context menu.

--- src/view/filetree/ContextMenu.java
+++ src/view/filetree/ContextMenu.java
@@ -4,15 +4,21 @@ import javax.swing.*;
 
 public class ContextMenu extends JPopupMenu {
     private JMenuItem _miView;
+    private JMenuItem _miEdit;
+    private JMenuItem _miDelete;
     private JMenuItem _miConstruct;
     private JMenuItem _miDeconstruct;
 
     public ContextMenu(){
         _miView = new JMenuItem("Просмотр");
+        _miEdit = new JMenuItem("Редактировать...");
+        _miDelete = new JMenuItem("Удалить");
         _miConstruct = new JMenuItem("Собрать");
         _miDeconstruct = new JMenuItem("Разобрать...");
 
         this.add(_miView);
+        this.add(_miEdit);
+        this.add(_miDelete);
         this.add(new Separator());
         this.add(_miConstruct);
         this.add(_miDeconstruct);
