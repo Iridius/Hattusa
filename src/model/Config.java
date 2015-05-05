a4bc6c4cc490deffc3c5dfc3a88aece8bcbc101e
Iridius <irde@mail.ru>
2015-05-05 16:49:26 +0200

    M: Hattusa: read last project from config.

--- src/model/Config.java
+++ src/model/Config.java
@@ -1,17 +1,21 @@
 package model;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.soap.Node;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class Config {
     private final static Logger log = Logger.getLogger(Config.class.getName());
-    private final static Path _configPath = Paths.get(System.getProperty("user.home"), "hattusa.cfg");
+    //private final static Path _configPath = Paths.get(System.getProperty("user.home"), "hattusa.cfg");
+    private final static Path _configPath = Paths.get("hattusa.xml");
 
     private static Config _CONFIG;
     private static Path _lastProjectPath;
@@ -30,13 +34,25 @@ public class Config {
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(_configPath.toFile());
+            File file = _configPath.toAbsolutePath().toFile();
+            if(!file.exists()) {
+                throw new Exception("Config file not exist.");
+            }
+            Document document = dBuilder.parse(file);
+            document.getDocumentElement().normalize();
 
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("lastproject");
-            if(nList.getLength() > 0) {
-                _lastProjectPath = Paths.get(nList.item(0).getNodeValue().toString());
+            NodeList childNodes = document.getDocumentElement().getChildNodes();
+            if(childNodes.getLength() > 0) {
+                for(int i = 0; i < childNodes.getLength(); i++) {
+                    if(childNodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                        Element element = (Element)childNodes.item(i);
 
+                        if(element.getNodeName().equals("lastproject")) {
+                            String value = element.getTextContent();
+                            _lastProjectPath = Paths.get(value);
+                        }
+                    }
+                }
             }
         } catch (Exception e) {
             log.severe(e.getMessage());
