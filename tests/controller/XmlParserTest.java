04783376c2807b6cf15a0035bb13a719ecea8036
Iridius <irde@mail.ru>
2015-06-05 12:16:41 +0200

    M: XmlParser: delete unused code.

--- tests/controller/XmlParserTest.java
+++ tests/controller/XmlParserTest.java
@@ -9,34 +9,6 @@ import static org.junit.Assert.*;
 
 public class XmlParserTest {
     private final String getLiterature() {
-//        return "<?xml version=\"1.0\" encoding=\"windows-1251\"?>\n" +
-//                "<attributes>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>{@CurrentPage}</name>\n" +
-//                "\t\t<value>Литература</value>\n" +
-//                "\t</attribute>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>{$Breadcrumbs}</name>\n" +
-//                "\t\t<value>{$Breadcrumbs}/mainpath.thtml</value>\n" +
-//                "\t</attribute>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>{$MainMenu}</name>\n" +
-//                "\t\t<value>{$Templates}/mm_literature.template</value>\n" +
-//                "\t</attribute>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>output</name>\n" +
-//                "\t\t<value>true</value>\n" +
-//                "\t</attribute>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>{$MainTemplate}</name>\n" +
-//                "\t\t<value>{$Templates}/main.template</value>\n" +
-//                "\t</attribute>\n" +
-//                "\t<attribute>\n" +
-//                "\t\t<name>{$Content}</name>\n" +
-//                "\t\t<value>{$BasePath}/Literature/Para/</value>\n" +
-//                "\t\t<filter>thtml</filter>\n" +
-//                "\t</attribute>\n" +
-//                "</attributes>";
         return "<?xml version=\"1.0\" encoding=\"windows-1251\"?>\n" +
                 "<attributes>\n" +
                 "\t<output>true</output>\n" +
@@ -51,13 +23,6 @@ public class XmlParserTest {
                 "\t\t<path>{$BasePath}\\literatute.thtml</path>\n" +
                 "\t</Content>\n" +
                 "</attributes>";
-    }
-    private final String getBlank() {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" +
-                "<book>\n" +
-                "\t<from><![CDATA[<li>]]></from>\n" +
-                "\t<to><![CDATA[</li>]]></to>\n" +
-                "</book>";
     }
 
     @Test
